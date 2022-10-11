701e2ce81b0da0057d5b7e65acec0979f75a08dd
Ulysse Valdenaire <ulysse.valdenairepro@gmail.com>
2022-10-11 19:16:09 +0200

    end game

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 from tkinter import Tk, Canvas
 import random
 
-from matplotlib.pyplot import eventplot
-
 SIZE = 300
 GAME = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
 
-def displayGrid(c="black"):
+def display_grid(c="black"):
     canvas.create_line((100, 0), (100, 300), width=3, fill=c)
     canvas.create_line((200, 0), (200, 300), width=3, fill=c)
     canvas.create_line((0, 100), (300, 100), width=3, fill=c)
     canvas.create_line((0, 200), (300, 200), width=3, fill=c)
-    
-    
-def displayPawns():
+
+
+def display_pawns():
     for x in range(3):
         for y in range(3):
             xx = x * 100
@@ -23,7 +21,7 @@ def displayPawns():
             B = (xx + 80, yy + 80)
             C = (xx + 20, yy + 80)
             D = (xx + 80, yy + 20)
-            
+
             if GAME[x][y] == 1:
                 canvas.create_oval(A, B, fill="blue")
             if GAME[x][y] == 2:
@@ -31,70 +29,75 @@ def displayPawns():
                 canvas.create_line(C, D, fill="red", width=10)
 
 
-def checkWon():
+def check_won():
     for j in [1, 2]:
         for x in range(3):
-            if GAME[x][0] == GAME[x][1] == GAME[x][2] == j : return j
+            if GAME[x][0] == GAME[x][1] == GAME[x][2] == j:
+                return j
         for y in range(3):
-            if GAME[0][y] == GAME[1][y] == GAME[2][y] == j : return j
-        if GAME[0][0] == GAME[1][1] == GAME[2][2] == j : return j
-        if GAME[0][2] == GAME[1][1] == GAME[2][0] == j : return j
-        
-        
-def findEmptyCases():
+            if GAME[0][y] == GAME[1][y] == GAME[2][y] == j:
+                return j
+        if GAME[0][0] == GAME[1][1] == GAME[2][2] == j:
+            return j
+        if GAME[0][2] == GAME[1][1] == GAME[2][0] == j:
+            return j
+
+
+def find_empty_cases():
     L = []
     for x in range(3):
         for y in range(3):
             if GAME[x][y] == 0:
                 L.append((x, y))
-                
-    if len(L) == 0: return False  
+
+    if len(L) == 0:
+        return False
     else:
         i = random.randint(0, len(L) - 1)
         return L[i]
 
 
-def programLoop():
-    displayGrid()
-
+def program_loop():
+    display_grid()
 
 
 def display():
     canvas.delete("all")
-    displayGrid()
-    displayPawns()
-                
+    display_grid()
+    display_pawns()
+
+
+def init_game():
+    global GAME
+    GAME = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    display()
+
 
 def click(event):
     global GAME
     display()
     x = event.x // 100
     y = event.y // 100
-    
-    # if checkWon() != 0:
-    #     GAME = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-    #     display()
-    #     return 
-    
-    if GAME[x][y] != 0 : return 
-    
+
+    if GAME[x][y] != 0:
+        return
+
     GAME[x][y] = 1
-    displayPawns()
-    if checkWon() == 1:
-        displayGrid("blue")
-        return 
-    
-    computer_turn = findEmptyCases()
+    display_pawns()
+    if check_won() == 1:
+        display_grid("blue")
+        window.after(3000, init_game)
+        return
+
+    computer_turn = find_empty_cases()
     if computer_turn != False:
         x, y = computer_turn
         GAME[x][y] = 2
-        displayPawns()
-        if checkWon() == 2:
-            displayGrid("red")
-            return 
-
-
-
+        display_pawns()
+        if check_won() == 2:
+            display_grid("red")
+            window.after(3000, init_game)
+            return
 
 
 window = Tk()
@@ -102,6 +105,6 @@ window.geometry(str(SIZE) + "x" + str(SIZE))
 canvas = Canvas(window, width=SIZE, height=SIZE, borderwidth=0,
                 highlightthickness=0, bg="lightgray")
 canvas.pack()
-window.after(100, programLoop)
+window.after(100, program_loop)
 window.bind("<Button-1>", click)
 window.mainloop()
